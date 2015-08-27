3fbba474556167f656c37b47ccb86f03d0509eaa
Anton Petrunich <anton.petrunich@gmail.com>
2015-08-27 21:21:57 +0300

    Add unicorn config

---
+++ config/unicorn.rb
@@ -0,0 +1,23 @@
+# Set the working application directory
+# working_directory "/path/to/your/app"
+working_directory "/home/rails/dispatcher"
+
+# Unicorn PID file location
+# pid "/path/to/pids/unicorn.pid"
+pid "/home/rails/dispatcher/tmp/pids/unicorn.pid"
+
+# Path to logs
+# stderr_path "/path/to/log/unicorn.log"
+# stdout_path "/path/to/log/unicorn.log"
+stderr_path "/home/rails/dispatcher/log/unicorn.log"
+stdout_path "/home/rails/dispatcher/unicorn.log"
+
+# Unicorn socket
+listen "/tmp/unicorn.dispatcher.sock"
+
+# Number of processes
+worker_processes 4
+worker_processes 2
+
+# Time-out
+timeout 30
